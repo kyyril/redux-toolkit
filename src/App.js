6cd710f26dd229c1955c8ffa52733dcb92fa3769
kyyril <khairilrahman55@gmail.com>
2024-09-29 02:04:40 +0700

    complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import Counter from "./components/Counter";
+import { Counter } from "./components/Counter";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Counter />
-    </div>
+    </>
   );
 }
 
---
+++ src/store/slices/counterSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment(state) {
+      state.value++;
+    },
+    decrement(state) {
+      state.value--;
+    },
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+export const counterReducer = counterSlice.reducer;
